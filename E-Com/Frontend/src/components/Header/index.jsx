8bef559390591bfd1a04b4cce6b35b63fa17757c
Tejas mehar <tejas mehar7@gmail.com>
2025-12-01 09:38:23 +0530

    Main Commit of This project

---
+++ E-Com/Frontend/src/components/Header/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import "../../../src/index.css";
+const Header = () => {
+    return (
+        <header>
+        <div className="top-strip py-5">
+            <div className="container">
+                 <div className="flex items-center justify-between ">
+                       <div className="col1 w-[50%]">
+                        <p className='text-[14px] '> Get 50% off new session styles,limited time only </p>
+                       </div>
+                 </div>
+            </div>
+        </div>
+        </header>
+    )
+}
+
+export default Header;
