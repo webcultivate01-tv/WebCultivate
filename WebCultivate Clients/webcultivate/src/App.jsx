2e6ac15d5907ee8f856aa9b2bdbff83f4b822a83
Tejas mehar <tejas mehar7@gmail.com>
2025-12-24 12:07:32 +0530

    Merge remote main with local project

--- WebCultivate Clients/webcultivate/src/App.jsx
+++ WebCultivate Clients/webcultivate/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import ScrollToTop from "./components/ScrollToTop";
